870eed118972f9493ef088282467c7c505d2ae6c
1237 <mamedov_vs@oksw.ru>
2019-05-06 13:32:17 +0700

    Создание программы и описания

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+The program takes data to the address of the form 
+https://devman.org/api/challenges/solution_attempts/?page=2.
+In the data obtained selects records in which the time in the range 
+from 00 hours to 6 am. The result is a list of login and time.
 
+#Examples
+
+```bash
+$ python bars.py seek_dev_nighters.py# possibly requires call of python3 executive instead of just python\
+Пользователь: id57193343 отправил 05:18:42 2019.05.04
+Пользователь: vmamedov1984 отправил 01:16:35 2019.04.30
+Пользователь: ZaurAgamov отправил 00:24:05 2019.04.20
+...
+```
+Running on Windows is similar.
+
+If no matching entries were found, a corresponding message will be displayed.
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,53 @@
+from datetime import datetime
+import pytz
+import requests
+
+
+URL = 'http://devman.org/api/challenges/solution_attempts/?page={}'
+
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    page = 1
+    while True:
+        response = requests.get(URL.format(page))
+        page += 1
+        if not response.ok:
+            return False
+        decoded_json = response.json()
+        yield from decoded_json['records']
+
+
+def get_local_date(timestamp, timezone):
+    tz = pytz.timezone(timezone)
+    local_date = datetime.fromtimestamp(timestamp, tz=tz)
+    return local_date
+
 
 def get_midnighters():
-    pass
+    midnighters = {}
+    for record in load_attempts():
+        timezone = record['timezone']
+        timestamp = record['timestamp']
+        username = record['username']
+        local_date = get_local_date(timestamp, timezone)
+        if local_date.hour <= 6:
+            midnighters[username] = local_date.strftime('%H:%M:%S %Y.%m.%d')
+    return midnighters
+
+
+def print_midnighters(midnighters):
+    if not len(midnighters):
+        print('Нет ни одного полуночника')
+        return
+
+    for username, local_date in midnighters.items():
+        print('Пользователь: {} отправил {}'.format(username, local_date))
+
+
+def main():
+    midnighters = get_midnighters()
+    print_midnighters(midnighters)
+
 
 if __name__ == '__main__':
-  pass
+  main()
